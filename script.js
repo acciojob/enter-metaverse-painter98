303d7eed070457b0dbd6650d8cae3b91f9481307
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-14 18:27:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 function enter(){
 	document.querySelector('h1').innerText='Entered Metaverse';
-	document.querySelector('#status').innerText='';
+	document.querySelector('#status').innerText=''; 
 }
